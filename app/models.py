cf9b1f33c938abe9b9489439962e46ac357f4a05
MaikoMori <fko2347019@stu.o-hara.ac.jp>
2024-10-24 12:12:12 +0900

    新規登録画面　電話番号追加

--- app/models.py
+++ app/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    # 電話番号フィールドを追加
+    phone = models.CharField(max_length=15, blank=False)  # ハイフンを含む電話番号用のフィールド
+
+    def __str__(self):
+        return self.username
